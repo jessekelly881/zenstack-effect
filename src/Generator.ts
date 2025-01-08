0f83a1a21a9af804c97f91799b86f996fe26d27f
Jesse Kelly <kellyjesse881@gmail.com>
2025-01-08 15:46:07 +0100

    converted index/run to effect

---
+++ src/Generator.ts
@@ -0,0 +1,1 @@
+import { FileSystem } from "@effect/platform"
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { NodeRuntime } from "@effect/platform-node";
 import type { DMMF } from '@prisma/generator-helper';
 import { PluginOptions } from '@zenstackhq/sdk';
 import { isDataModel, Model } from '@zenstackhq/sdk/ast';
 import { config } from 'dotenv';
+import { Config, Effect } from "effect";
 import { astToString, modelAst } from "./Ast";
 
 config();
@@ -9,15 +11,17 @@ config();
 export const name = 'ZenStack Effect Schema';
 export const description = 'Generate Effect Schemas from ZenStack';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default async function run(model: Model, options: PluginOptions, dmmf: DMMF.Document) {
-    if (process.env.DISABLE_ZENSTACK_EFFECT === 'true' || options.disable) {
-        return;
+const run = (model: Model, options: PluginOptions, dmmf: DMMF.Document) => Effect.gen(function*() {
+    const isDisabled = yield* Config.boolean("DISABLE_ZENSTACK_EFFECT").pipe(Config.withDefault(false))
+    if(isDisabled) {
+        return
     }
 
     const dataModels = model.declarations.filter(isDataModel);
 
-    // const outFolder = resolvePath((options.output as string) ?? './effect', options);
     const ast = modelAst(dataModels[0]);
-    console.log(astToString(ast))
-}
+    console.log(`model: ${astToString(ast)}`)
+
+}).pipe(NodeRuntime.runMain)
+
+export default run
