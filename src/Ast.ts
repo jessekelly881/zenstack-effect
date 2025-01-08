837bd5837ac5e7ec579a9c41ca8c9ab6ffacc334
Jesse Kelly <kellyjesse881@gmail.com>
2025-01-08 15:33:41 +0100

    refactor: moved ts ast fns to Ast.ts

---
+++ src/Ast.ts
@@ -0,0 +1,134 @@
+import ts, { factory } from "typescript";
+
+import type {
+	BuiltinType,
+	DataModel,
+	DataModelField,
+} from "@zenstackhq/sdk/ast";
+import { Match } from "effect";
+
+/**
+ * Convert an AST to a string
+ * @internal
+ */
+export const astToString = (
+	nodes: ts.Node | ts.Node[],
+	printerOptions?: ts.PrinterOptions,
+) => {
+	const sourceFile = ts.createSourceFile(
+		"print.ts",
+		"",
+		ts.ScriptTarget.Latest,
+		false,
+		ts.ScriptKind.TS,
+	);
+	const printer = ts.createPrinter(printerOptions);
+
+	const output = printer.printList(
+		ts.ListFormat.MultiLine,
+		ts.factory.createNodeArray(Array.isArray(nodes) ? nodes : [nodes]),
+		sourceFile,
+	);
+
+	return output;
+};
+
+/**
+ * Import declaration for `import { Schema } from "effect"`
+ * @internal
+ */
+export const schemaImportAst = factory.createImportDeclaration(
+	undefined,
+	factory.createImportClause(
+		false,
+		undefined,
+		factory.createNamedImports([
+			factory.createImportSpecifier(
+				false,
+				undefined,
+				factory.createIdentifier("Schema"),
+			),
+		]),
+	),
+	factory.createStringLiteral("effect"),
+	undefined,
+);
+
+/**
+ * Generates a TypeScript AST for a `BuiltinType`. Returns a `ts.PropertyAccessExpression`.
+ */
+export const builtInTypeAst = (type: BuiltinType) =>
+	factory.createPropertyAccessExpression(
+		factory.createIdentifier("Schema"),
+		Match.value(type).pipe(
+			Match.when("BigInt", () => factory.createIdentifier("BigInt")),
+			Match.when("Boolean", () => factory.createIdentifier("Boolean")),
+			Match.when("Int", () => factory.createIdentifier("Int")),
+			Match.when("Float", () => factory.createIdentifier("Number")),
+			Match.when("String", () => factory.createIdentifier("String")),
+			Match.when("Json", () => factory.createIdentifier("Object")),
+			Match.when("Decimal", () => factory.createIdentifier("Number")),
+			Match.when("Bytes", () => factory.createIdentifier("Uint8Array")),
+			Match.when("DateTime", () => factory.createIdentifier("DateTimeUtc")),
+			Match.exhaustive,
+		),
+	);
+
+/**
+ * Generates a TypeScript AST for a `DataModelField`. Returns a `ts.PropertyAssignment`.
+ */
+export const fieldAst = (field: DataModelField) => {
+	const value = factory.createPropertyAccessExpression(
+		factory.createIdentifier("Schema"),
+		factory.createIdentifier("String"),
+	);
+
+	return factory.createPropertyAssignment(
+		factory.createIdentifier(field.name),
+		value,
+	);
+};
+
+/**
+ * Generates a TypeScript AST for a `Schema` from a DataModel
+ */
+export const modelAst = (
+	model: DataModel,
+	options: { export?: boolean } = {},
+) =>
+	factory.createClassDeclaration(
+		options.export ? [factory.createToken(ts.SyntaxKind.ExportKeyword)] : [],
+		factory.createIdentifier(model.name),
+		undefined,
+		[
+			factory.createHeritageClause(ts.SyntaxKind.ExtendsKeyword, [
+				factory.createExpressionWithTypeArguments(
+					factory.createCallExpression(
+						factory.createCallExpression(
+							factory.createPropertyAccessExpression(
+								factory.createIdentifier("Schema"),
+								factory.createIdentifier("TaggedClass"),
+							),
+							[
+								factory.createTypeReferenceNode(
+									factory.createIdentifier(model.name),
+									undefined,
+								),
+							],
+							[],
+						),
+						undefined,
+						[
+							factory.createStringLiteral(model.name),
+							factory.createObjectLiteralExpression(
+								model.fields.map((field) => fieldAst(field)),
+								true,
+							),
+						],
+					),
+					undefined,
+				),
+			]),
+		],
+		[],
+	);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import type { DMMF } from '@prisma/generator-helper';
-import { PluginOptions, resolvePath } from '@zenstackhq/sdk';
-import { Model } from '@zenstackhq/sdk/ast';
+import { PluginOptions } from '@zenstackhq/sdk';
+import { isDataModel, Model } from '@zenstackhq/sdk/ast';
 import { config } from 'dotenv';
+import { astToString, modelAst } from "./Ast";
 
 config();
 
@@ -10,10 +11,13 @@ export const description = 'Generate Effect Schemas from ZenStack';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default async function run(model: Model, options: PluginOptions, dmmf: DMMF.Document) {
-    if (process.env.DISABLE_ZENSTACK_MD === 'true' || options.disable) {
+    if (process.env.DISABLE_ZENSTACK_EFFECT === 'true' || options.disable) {
         return;
     }
 
-    const outFolder = resolvePath((options.output as string) ?? './effect', options);
-    console.log(`Generating Effect Schemas to ${outFolder}`);
+    const dataModels = model.declarations.filter(isDataModel);
+
+    // const outFolder = resolvePath((options.output as string) ?? './effect', options);
+    const ast = modelAst(dataModels[0]);
+    console.log(astToString(ast))
 }
